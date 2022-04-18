6514324aa13c08b157f9a6e4bfff9c10acab996f
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2022-04-18 16:33:04 -0400

    adding the table the navBar and the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
+import { CreateEmpleadosComponent } from './components/create-empleados/create-empleados.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'list-empleados', pathMatch: 'full' },
+  {path: 'list-empleados', component: ListEmpleadosComponent},
+  {path: 'create-empleado', component: CreateEmpleadosComponent},
+  {path: '**', redirectTo: 'list-empleados', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

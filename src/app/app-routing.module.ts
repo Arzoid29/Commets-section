a59774956bd9ad77a6b9a044aa4c843e58a8550f
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2022-04-21 16:50:50 -0400

    adding the register form and login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 import { CreateEmpleadosComponent } from './components/create-empleados/create-empleados.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 const routes: Routes = [
   { path: '', redirectTo: 'list-empleados', pathMatch: 'full' },
   {path: 'list-empleados', component: ListEmpleadosComponent},
   {path: 'create-empleado', component: CreateEmpleadosComponent},
+  {path: 'Login', component: LoginComponent},
+  {path: 'Register', component: RegisterComponent},
   {path: '**', redirectTo: 'list-empleados', pathMatch: 'full'},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 import { CreateEmpleadosComponent } from './components/create-empleados/create-empleados.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListEmpleadosComponent,
     CreateEmpleadosComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

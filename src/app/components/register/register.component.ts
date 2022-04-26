7025bdbf1c0ed94cd4e33f3c90720e02811c4ea4
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2022-04-26 16:55:42 -0400

    making the register works

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  addUser = async(e: any): Promise<void> => {
+    e.preventDefault();
+    const person = e.target;
+    const res = await fetch('http://localhost:3000/api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: person[0].value,
+        email: person[2].value,
+        password: person[3].value,
+        lastName:  person[1].value
+      }),
+
+    });
+    const data = await res.json();
+    console.log(data);
+    e.target.reset();
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    // const res = await fetch('http://localhost:3000/api/v1/users', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     name: 'John Doe',
+    //     email: 'john@gmail.com',
+    //     password: 'QWERTY1234',
+    //     lastName: 'Doe'
+    //   }),
+
+    // });
+    // const data = await res.json();
+    // console.log(data);
+
   }
 
 }

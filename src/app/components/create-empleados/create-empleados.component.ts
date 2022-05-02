a5fc9b125a986eb8ead4d82420d877bbfb7ad70e
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2022-05-02 15:23:15 -0400

    Making the add employees work

--- src/app/components/create-empleados/create-empleados.component.ts
+++ src/app/components/create-empleados/create-empleados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-create-empleados',
@@ -6,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-empleados.component.css']
 })
 export class CreateEmpleadosComponent implements OnInit {
+  addEmployee = async(e: any): Promise<void> => {
+    e.preventDefault();
 
+    const employee = e.target;
+    const res = await fetch('http://localhost:3000/api/v1/addEmployees', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: employee[0].value,
+        documentId: employee[2].value,
+        salary: employee[3].value,
+        lastName:  employee[1].value
+      }),
+
+    });
+    const data = await res.json();
+    if ( data.status === 201) {
+      Swal.fire({
+        title: 'Success',
+        text: 'You have successfully registered',
+        icon: 'success'
+      });
+    }
+    else {
+      Swal.fire({
+        title: 'Error',
+        text: 'Something went wrong',
+        icon: 'error'
+      });
+    }e.target.reset();
+  }
   constructor() { }
 
   ngOnInit(): void {
